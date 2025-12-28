ea3dd651ed1152b7e4cfb10e340d6f699f50a756
shivanshgupta2504 <shivanshgupta2057@gmail.com>
2025-12-28 13:47:37 +0530

    Update tut-4

--- tut-4/app.py
+++ tut-4/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response, send_from_directory, jsonify
+import pandas as pd
+import os
+import uuid
 
 app = Flask(__name__, template_folder="templates")
 
@@ -16,8 +19,58 @@ def index():
 
 @app.route("/file_upload", methods=['POST'])
 def file_upload():
-    file = request.form.get("file", "no file")
+    file = request.files['file']
+
+    if file.content_type == "text/plain":
+        return file.read().decode("utf-8")
+    elif file.content_type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+        df = pd.read_excel(file)
+        return df.to_html()
     return f"You have uploaded a file"
+
+@app.route("/convert_csv", methods=['POST'])
+def convert_csv():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+    response = Response(
+        df.to_csv(index=False),
+        mimetype="text/csv",
+        headers={
+            "Content-Disposition": "attachment; filename=result.csv",
+        }
+    )
+
+    return response
+
+@app.route("/convert_csv_two", methods=['POST'])
+def convert_csv_two():
+    file = request.files['file']
+
+    df = pd.read_excel(file)
+
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+
+    filename = f"{uuid.uuid4()}.csv"
+
+    df.to_csv(os.path.join("downloads", filename))
+
+    return render_template("downloads.html", filename=filename)
+
+@app.route("/download_csv/<filename>")
+def download_csv(filename):
+    return send_from_directory("downloads", filename, download_name="result.csv")
+
+@app.route("/handle_post", methods=['POST'])
+def handle_post():
+    greeting = request.json["greeting"]
+    name = request.json["name"]
+
+    with open("example.txt", "w") as f:
+        f.write(f"{greeting} {name}")
+
+    return jsonify({"message": "successfully written"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
